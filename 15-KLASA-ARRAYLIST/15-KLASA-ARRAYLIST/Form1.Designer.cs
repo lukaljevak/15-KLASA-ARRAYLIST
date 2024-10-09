36797107221cd9fd5f0773ec6c28b7a64feebce1
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-10-09 09:22:27 +0200

    Add files via upload

---
+++ 15-KLASA-ARRAYLIST/15-KLASA-ARRAYLIST/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace _15_KLASA_ARRAYLIST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonNovi = new System.Windows.Forms.Button();
+            this.buttonUnesi = new System.Windows.Forms.Button();
+            this.textBoxNovi = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonObrisi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonNovi
+            // 
+            this.buttonNovi.Location = new System.Drawing.Point(120, 55);
+            this.buttonNovi.Name = "buttonNovi";
+            this.buttonNovi.Size = new System.Drawing.Size(148, 39);
+            this.buttonNovi.TabIndex = 0;
+            this.buttonNovi.Text = "Novi automobil";
+            this.buttonNovi.UseVisualStyleBackColor = true;
+            this.buttonNovi.Click += new System.EventHandler(this.buttonNovi_Click);
+            // 
+            // buttonUnesi
+            // 
+            this.buttonUnesi.Location = new System.Drawing.Point(332, 144);
+            this.buttonUnesi.Name = "buttonUnesi";
+            this.buttonUnesi.Size = new System.Drawing.Size(110, 35);
+            this.buttonUnesi.TabIndex = 1;
+            this.buttonUnesi.Text = "Unesi";
+            this.buttonUnesi.UseVisualStyleBackColor = true;
+            this.buttonUnesi.Click += new System.EventHandler(this.buttonUnesi_Click);
+            // 
+            // textBoxNovi
+            // 
+            this.textBoxNovi.Enabled = false;
+            this.textBoxNovi.Location = new System.Drawing.Point(120, 144);
+            this.textBoxNovi.Name = "textBoxNovi";
+            this.textBoxNovi.Size = new System.Drawing.Size(171, 26);
+            this.textBoxNovi.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(120, 199);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(322, 241);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // buttonObrisi
+            // 
+            this.buttonObrisi.Location = new System.Drawing.Point(120, 498);
+            this.buttonObrisi.Name = "buttonObrisi";
+            this.buttonObrisi.Size = new System.Drawing.Size(148, 53);
+            this.buttonObrisi.TabIndex = 4;
+            this.buttonObrisi.Text = "Obriši listu";
+            this.buttonObrisi.UseVisualStyleBackColor = true;
+            this.buttonObrisi.Click += new System.EventHandler(this.buttonObrisi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 586);
+            this.Controls.Add(this.buttonObrisi);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBoxNovi);
+            this.Controls.Add(this.buttonUnesi);
+            this.Controls.Add(this.buttonNovi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonNovi;
+        private System.Windows.Forms.Button buttonUnesi;
+        private System.Windows.Forms.TextBox textBoxNovi;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonObrisi;
+    }
+}
+
---
+++ 15-KLASA-ARRAYLIST/15-KLASA-ARRAYLIST/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace _15_KLASA_ARRAYLIST
+{
+    public partial class Form1 : Form
+    {
+        ArrayList listaAutomobila = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+            IsključiKontrole();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonNovi_Click(object sender, EventArgs e)
+        {
+            UključiKontrole();
+            textBoxNovi.Clear();
+        }
+
+        private void buttonUnesi_Click(object sender, EventArgs e)
+        {
+            Automobil a = new Automobil(textBoxNovi.Text);
+            listaAutomobila.Add(a);
+            richTextBox1.AppendText((listaAutomobila.IndexOf(a) + 1) + ". " + a.DajMarku() + "\n");
+            IsključiKontrole();
+        }
+
+        private void buttonObrisi_Click(object sender, EventArgs e)
+        {
+            listaAutomobila.Clear();
+            richTextBox1.Clear();
+        }
+        private void IsključiKontrole()
+        {
+            textBoxNovi.Enabled = false;
+            buttonUnesi.Enabled = false;
+        }
+
+        private void UključiKontrole()
+        {
+            textBoxNovi.Enabled = true;
+            buttonUnesi.Enabled = true;
+        }
+        class Automobil
+
+        {
+
+            private string marka;
+
+            public Automobil(string m)
+
+            {
+
+                marka = m;
+
+            }
+
+            public string DajMarku()
+
+            {
+
+                return marka;
+
+            }
+
+        }
+    }
+}
